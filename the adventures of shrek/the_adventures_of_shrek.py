791a1ac0ab8d41bfad25505530d6dada06b5fc2a
Tyler Stimpson <tyler.stimpson@horowhenua.school.nz>
2026-02-24 12:32:36 +1300

    Created shield class

--- the adventures of shrek/the_adventures_of_shrek.py
+++ the adventures of shrek/the_adventures_of_shrek.py
@@ -7,3 +7,11 @@ def __init__(self, name, description, power):
 
 def __str__(self):
         return f"{self.name}: {self.description} (Power: {self.power})"
+
+class Sheild:
+    def __init__(self, name, description, defense):
+        self.name = name
+        self.description = description
+        self.defense = defense
+    def __str__(self):
+        return f"{self.name}: {self.description}. Power: {self.power}"
