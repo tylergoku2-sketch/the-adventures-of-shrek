6d8b99834839416b50ce507e3fa4f4ab4f23789a
Tyler Stimpson <tyler.stimpson@horowhenua.school.nz>
2026-02-27 08:55:46 +1300

    Created location class

--- the adventures of shrek/the_adventures_of_shrek.py
+++ the adventures of shrek/the_adventures_of_shrek.py
@@ -23,3 +23,31 @@ class Sheild:
             self.attack = attack
             self.defense = defense
             self.description = description
+
+class Location:
+    def __init__(self, name, description):
+        self.name = name
+        self.monster = 0
+        self.description = description
+        self.items = []
+        self.exits = []
+
+def link(self, other, direction):
+        self.exits[direction] = other
+
+def enter(self):
+    pass
+
+def examine(self, thing):
+    print(f"There is no {thing} here")
+
+def describe(self):
+    print(f"\n== {self.name} here")
+    print(self.description)
+    if len(self.items) > 0:
+        print("\nYou see:")
+        for item in self.items:
+            print(f" - {item}")
+            if self.monster != 0:
+                print(f"\nA {self.monster.name} is here! {self.description}")
+      
